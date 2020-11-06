ca4f9d9cb1a084c573e823e21b3e83012dcdc2a9
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-11-05 22:48:36 -0500

    Adding react router and page routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import Home from './pages/Homepage';
+// import Home from './pages/Homepage';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Portfolio from './Components/Portfolio';
+import About from './Components/About';
 
-const App = () => {
+
+
+
+function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+
+        <Route exact path="/" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        {/* <Route exact path="/" component={Projects} /> */}
+
+
+        <div className="navigation">
+          {/* <img src="" className="logo" alt="Logo Image" /> */}
+          <div className="navigation-sub">
+
+
+            <Link to="/" className="item m-2">About</Link>
+            <Link to="/portfolio" className="item m-2">Portfolio</Link>
+            {/* <a href="" className="item">About</a> */}
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
+
   );
 }
 
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 
-function Header () {
+function Header() {
     return (
-        <div className="header jumbotron text-center">
+        <div className="header text-center">
             <h1>Alexander M. Bertrand</h1>
         </div>
     )
