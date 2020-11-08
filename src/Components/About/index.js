759233133ed3cf4d8bde7243d01cadaabb67668a
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-11-08 00:05:36 -0500

    About component working!

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -3,14 +3,14 @@ import Image from '../../img/Alex-Bertrand.jpg';
  
 function About(props) {
   return (
-    <div className="container">
-      <section className="container-fluid bg-secondary m-3 p-3">
+    <div className="container bg-secondary">
+      <section className="container-fluid m-3 pb-5">
             <div className="row">
                 <div className="col-12">
-                    <h2 className="p-3">About Me</h2>
+                    <h2 className="p-3 text-white">About Me</h2>
                     <hr />
                     <img className="float-left mr-3" height="320" src={Image} alt="Alex" />
-                    <p> Aspiring Full-stack web developer with event management experience looking to leverage my strong
+                    <p className="text-white"> Aspiring Full-stack web developer with event management experience looking to leverage my strong
                         work ethic and ability to learn quickly. Currently studying in the University of New Hampshire’s
                         certificate program in full stack web development, where I am building my skills in HTML, CSS,
                         JavaScript and Node.js as well as responsive and mobile-first web design. Recently completed a
@@ -21,7 +21,7 @@ function About(props) {
                         applications and
                         software come to life.
                     </p>
-                    <p>In my free time when I am not building great web experiences, I enjoy playing golfing, skiing and
+                    <p className="text-white">In my free time when I am not building great web experiences, I enjoy playing golfing, skiing and
                         running.
                     </p>
                     <br />
