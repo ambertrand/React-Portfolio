0cc9a7843f464714083d95595ddfb97e3192b5f8
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-11-08 10:32:52 -0500

    updating background image

--- src/Components/Project/index.js
+++ src/Components/Project/index.js
@@ -1,13 +1,15 @@
 import React from "react"
 import Thumbnail from '../Thumbnail';
+import projectThumbnail from '../../img/index-screenshot.png';
  
 function Projects() {
   return (
     <div className="container bg-secondary mt-3">
-      <h1 className=" display-4 text-center text-white">Projects</h1>
+      <h1 className=" display-4 text-white">Projects</h1>
+      <hr />
       <Thumbnail
         link="/whispering-castle-57159.herokuapp.com/"
-        image="./img/index-screenshot.png"
+        image= ""
         title="Outdoor Adventure Buddies"
         category="Project"
       />
