fce91ee97ca6e30a177b0f0e66a18232df7695ac
Alex Bertrand <alex.m.bertrand@gmail.com>
2021-01-03 16:14:25 -0500

    resetting react portfolio

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './Components/Navbar';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Header from './Components/Header';
 import Footer from './Components/Footer';
-
-import Home from './Views/Home';
-import About from './Views/About';
+import About from './Components/About';
 import Portfolio from './Components/Portfolio';
-import Contact from './Views/Contact';
+import Contact from './Components/ContactMe';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -14,23 +12,28 @@ import './App.css';
 function App() {
   return (
     <Router>
-      <>
-        <Navbar />
-        <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/about">
-          <About />
-        </Route>
-        <Route exact path="/portfolio" component={Portfolio} />
-        <Route exact path="/contact">
-          <Contact />
-        </Route>
 
-        </Switch>
+      <div className="App container-fluid">
+        <Header />
+
+        <div className="row navigation navbar bg-light d-flex">
+          <div className="col-12 navigation-sub text-center">
+
+            <Link to="/" className="item navbar-brand m-3">Home</Link>
+            <Link to="/about" className="item navbar-brand m-3">About Me</Link>
+            <Link to="/portfolio" className="item navbar-brand m-3">Projects</Link>
+            <Link to="/contact" className="item navbar-brand m-3">Contact</Link>
+
+          </div>
+        </div>
+
+        <Route exact path="/" />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/contact" component={Contact} />
+
         <Footer />
-      </>
+      </div>
     </Router>
   );
 }
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -5,7 +5,7 @@ import React from 'react';
 function Header() {
     return (
         <div>
-            <h1 className="display-3 text-center"><strong>Alexander M. Bertrand</strong></h1>
+            <h1 className="display-3 text-center text-white"><strong>Alexander M. Bertrand</strong></h1>
         </div>
     );
 }
